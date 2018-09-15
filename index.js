fff61fe4441f74acda127570f49d6c0fa073bc21
Talisa Erazo <talisaerazo@gmail.com>
2018-09-15 16:00:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function destructivelyRemoveFirstKitten(name){
   return kittens;
 }
 function appendKitten(name){
-  var newKittens = [...kittens]
-  newKittens.push(name)
-  return newKittens
+  var newKittens = [...kittens];
+  newKittens.push(name);
+  return newKittens;
 }
